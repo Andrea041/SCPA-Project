3da8e37e3d608188a94bc1a0f334ca6a7ab34d6a
PierfrancescoLijoi <pierfrancescp.lijoi@libero.it>
2024-12-23 12:33:14 +0100

    commit per openMP

--- prova.c
+++ prova.c
@@ -1,6 +1,16 @@
-//
-// Created by pierfrancesco on 12/23/24.
-//
-//ciao
+#include <stdio.h>
+#include <omp.h>
 
-#include <mpi.h>
+int main() {
+    const int N = 10; // Dimensione del lavoro
+    int i;
+
+    // Esecuzione parallela
+#pragma omp parallel for
+    for (i = 0; i < N; i++) {
+        int thread_id = omp_get_thread_num(); // Ottieni l'ID del thread corrente
+        printf("Thread ID: %d sta eseguendo l'iterazione %d\n", thread_id, i);
+    }
+
+    return 0;
+}
