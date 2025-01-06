4597eaeb04085256d33025e5517aaeb9e236faca
PierfrancescoLijoi <pierfrancescp.lijoi@libero.it>
2025-01-06 17:37:52 +0100

    semplice riscrittura della funzione disaccoppiata per funzionalità

--- src/csrOperations.c
+++ src/csrOperations.c
@@ -45,7 +45,38 @@ struct matrixPerformance serial_csr(struct matrixData *matrix_data, double *x) {
     return node;
 }
 
-/* Funzione per svolgere il prodotto matrice-vettore, con memorizzazione CSR della matrice, in modo parallelo con OpenMP */
+void compute_thread_row_partition(int M, int nz, int num_threads, int *IRP, int **start_row, int **end_row) {
+    *start_row = malloc((size_t)num_threads * sizeof(int));
+    *end_row = malloc((size_t)num_threads * sizeof(int));
+
+    if (*start_row == NULL || *end_row == NULL) {
+        printf("Errore nell'allocazione della memoria per la partizione delle righe\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int t = 0; t < num_threads; t++) {
+        (*start_row)[t] = -1;
+        (*end_row)[t] = -1;
+    }
+
+    int nnz_per_thread = nz / num_threads;
+    int current_nnz = 0, current_thread = 0;
+
+    for (int i = 0; i < M; i++) {
+        if (current_nnz >= current_thread * nnz_per_thread && (*start_row)[current_thread] == -1) {
+            (*start_row)[current_thread] = i;
+        }
+
+        if (current_nnz >= (current_thread + 1) * nnz_per_thread || i == M - 1) {
+            (*end_row)[current_thread] = i + 1; // Include l'ultima riga
+            current_thread++;
+            if (current_thread >= num_threads) break;
+        }
+
+        current_nnz += IRP[i + 1] - IRP[i];
+    }
+}
+
 struct matrixPerformance parallel_csr(struct matrixData *matrix_data, double *x) {
     int *IRP, *JA;
     double *AS;
@@ -64,38 +95,10 @@ struct matrixPerformance parallel_csr(struct matrixData *matrix_data, double *x)
 
     convert_to_csr(matrix_data->M, matrix_data->nz, matrix_data->row_indices, matrix_data->col_indices, matrix_data->values, &IRP, &JA, &AS);
 
-    int total_nonzeros = matrix_data->nz;
-
     int num_threads = omp_get_max_threads();
-    /* Solo per debug */
-    //printf("Numero massimo di threads su processore: %d\n", num_threads);
+    int *start_row, *end_row;
 
-    int nnz_per_thread = total_nonzeros / num_threads;
-
-    int *start_row = malloc((size_t)num_threads * sizeof(int));
-    int *end_row = malloc((size_t)num_threads * sizeof(int));
-
-    for (int t = 0; t < num_threads; t++) {
-        start_row[t] = -1;
-        end_row[t] = -1;
-    }
-
-    /* Suddivisione delle righe tra i thread */
-    int current_nnz = 0, current_thread = 0;
-    for (int i = 0; i < matrix_data->M; i++) {
-        if (current_nnz >= current_thread * nnz_per_thread && start_row[current_thread] == -1) {
-            start_row[current_thread] = i;
-        }
-
-        if (current_nnz >= (current_thread + 1) * nnz_per_thread || i == matrix_data->M - 1) {
-            end_row[current_thread] = i + 1; // Include l'ultima riga
-            /* Solo per debug */
-            //printf("Thread %d: Righe assegnate: %d - %d\n", current_thread, start_row[current_thread], end_row[current_thread]);
-            current_thread++;
-            if (current_thread >= num_threads) break;
-        }
-        current_nnz += IRP[i + 1] - IRP[i];
-    }
+    compute_thread_row_partition(matrix_data->M, matrix_data->nz, num_threads, IRP, &start_row, &end_row);
 
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
